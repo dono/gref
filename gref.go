861e871297c40072323f8d8919402fdd3aca7b4e
dono <xononeet@gmail.com>
2021-12-21 23:29:37 +0900

    update

--- gref.go
+++ gref.go
@@ -3,14 +3,33 @@ package main
 import (
 	"fmt"
 
-	"github.com/ktr0731/go-fuzzyfinder/matching"
+	"github.com/sahilm/fuzzy"
 )
 
 // ToDo: -v, -r, file reading, highlight
 func main() {
-	cases := []string{"hoge", "fuga", "test"}
-	matched := matching.FindAll("test", cases, matching.WithMode(matching.ModeSmart))
-	for _, v := range matched {
-		fmt.Println(cases[v.Idx])
+	cases := []string{"hoge", "fuga", "foo"}
+	matches := fuzzy.Find("zab", cases)
+
+	const bold = "\033[1m%s\033[0m"
+
+	for _, match := range matches {
+		for i := 0; i < len(match.Str); i++ {
+			if contains(i, match.MatchedIndexes) {
+				fmt.Printf(bold, string(match.Str[i]))
+			} else {
+				fmt.Print(string(match.Str[i]))
+			}
+		}
+		fmt.Println()
 	}
+}
+
+func contains(needle int, haystack []int) bool {
+	for _, i := range haystack {
+		if needle == i {
+			return true
+		}
+	}
+	return false
 }
